6052f59ebf2524a61d02b2f33450326a4213d0d0
Maja Francetic <francetic.maja@gmail.com>
2019-11-30 20:23:25 +0100

    Update UI for the activity_main.xml

--- app/src/main/java/com/mfrancetic/hikerswatch/MainActivity.java
+++ app/src/main/java/com/mfrancetic/hikerswatch/MainActivity.java
@@ -1,14 +1,31 @@
 package com.mfrancetic.hikerswatch;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Context;
+import android.graphics.drawable.BitmapDrawable;
+import android.media.Image;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Context context;
+
+    private ImageView mainBackgroundImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mainBackgroundImageView = findViewById(R.id.main_background_image_view);
+        context = mainBackgroundImageView.getContext();
+
+
+        Glide.with(context).load(R.drawable.forest).centerCrop().into(mainBackgroundImageView);
     }
 }
